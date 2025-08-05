e0bc0770c3d690aa11704084daa1701495b7bd13
mykill69 <baliviamichael@gmail.com>
2025-08-05 13:16:18 +0800

    update

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -46,7 +46,8 @@
                                             <tr>
                                                 {{-- <td>{{ $no++ }}.</td> --}}
                                                 <td></td>
-                                                <td>{{ ucfirst(strtolower($user->fname)) }} {{ ucfirst(strtolower($user->mname)) }} {{ strtoupper($user->lname) }}</td>
+                                               <td>{{ ucwords(strtolower($user->fname)) }} {{ ucwords(strtolower($user->mname)) }} {{ strtoupper($user->lname) }}</td>
+
 
                                                 <td class="text-bold text-primary">{{ $user->email }}</td>
                                                 <td>{{ ucfirst(strtolower($user->department)) }}</td>
