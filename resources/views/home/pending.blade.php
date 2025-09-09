2cefb94856cdcd11fbfc5ec59161f414b35bc66c
mykill69 <baliviamichael@gmail.com>
2025-09-09 09:30:36 +0800

    update

--- resources/views/home/pending.blade.php
+++ resources/views/home/pending.blade.php
@@ -171,6 +171,10 @@
         <!-- /.content -->
     </div>
 
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    
     <script>
         $(document).ready(function() {
             var t = $('#pendingTable').DataTable({
