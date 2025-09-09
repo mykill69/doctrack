c18e63d48ec6b646bca5d6e0146ef8674794636b
mykill69 <baliviamichael@gmail.com>
2025-09-09 09:33:48 +0800

    update

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -300,6 +300,10 @@
             </div>
         </div>
     </div>
+
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+      
     @if (!$dpa)
         <script>
             document.addEventListener("DOMContentLoaded", function() {
