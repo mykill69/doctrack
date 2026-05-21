606aa0b99bc325fded68a23a4f3f1b2defbecf74
mykill69 <baliviamichael@gmail.com>
2026-05-21 10:51:16 +0800

    update

--- resources/views/home/served.blade.php
+++ resources/views/home/served.blade.php
@@ -198,7 +198,6 @@
     @include('modal.addTrans')
     @include('modal.addRoutslip')
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <script>
     $(document).ready(function() {
