b595ca89b1e152a5d5c6580fddd1b433907febe5
mykill69 <baliviamichael@gmail.com>
2026-05-28 11:22:22 +0800

    update

--- resources/views/slip/routingSlip.blade.php
+++ resources/views/slip/routingSlip.blade.php
@@ -88,5 +88,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     @include('modal.addRoutslip')
     @include('modal.addDestination')
+
+
+
+
     
 @endsection
