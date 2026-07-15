98a3cb0ab309156dde85f142365da234375460a5
mykill69 <baliviamichael@gmail.com>
2026-07-15 15:26:47 +0800

    update

--- resources/views/print/logbookPdf.blade.php
+++ resources/views/print/logbookPdf.blade.php
@@ -1,7 +1,6 @@
 <style>
     @page {
         margin: 120px 40px 80px 40px;
-        /* top, right, bottom, left */
     }
 
     body {
@@ -10,7 +9,6 @@
         padding: 0;
     }
 
-    /* Header */
     header {
         position: fixed;
         top: -100px;
@@ -25,7 +23,6 @@
         height: auto;
     }
 
-    /* Footer */
     footer {
         position: fixed;
         bottom: -60px;
@@ -42,7 +39,6 @@
         display: block;
     }
 
-    /* Table */
     table.dashboardTable {
         width: 100%;
         border: 1px solid black;
@@ -57,13 +53,11 @@
         padding: 3px;
         text-align: left;
         height: 27px;
-        /* consistent row height */
         vertical-align: middle;
     }
 
     thead {
         display: table-header-group;
-        /* repeat on each page */
     }
 
     tr {
@@ -80,51 +74,62 @@
 </style>
 
 <body>
-    <!-- Header -->
     <header>
         <img src="{{ public_path('template/img/header_new.png') }}" alt="Header Image">
         <h3 style="margin-top: -5px;">DOCUMENT LOGBOOK</h3>
     </header>
 
-    <!-- Footer -->
     <footer>
         <img src="{{ public_path('template/img/footer-logbook.png') }}" alt="Footer Image">
     </footer>
 
     @php
-        $logsPerPage = 15;
+    $logsByRouteId = $logs->keyBy('route_id');
+    $minRouteId = $logs->min('route_id');
+    $maxRouteId = $logs->max('route_id');
+    
+    // Build complete sequence from min to max
+    $orderedLogs = collect();
+    for ($i = $minRouteId; $i <= $maxRouteId; $i++) {
+        $orderedLogs->push([
+            'route_id' => $i,
+            'log' => $logsByRouteId[$i] ?? null,
+        ]);
+    }
+    
+    $logsPerPage = 15;
+    $chunks = $orderedLogs->chunk($logsPerPage);
+    $totalPages = $chunks->count();
+@endphp
 
-        // Keep it as a collection
-        $chunks = $logs->values()->chunk($logsPerPage);
-
-        $totalPages = $chunks->count();
-    @endphp
-
-    @foreach ($chunks as $pageIndex => $chunk)
-        <div class="content">
-            <table class="dashboardTable">
-                <thead>
-                    <tr>
-                        <th style="width: 2%;">CTRL #</th>
-                        <th style="width: 6%;">DATE RECEIVED</th>
-                        <th style="width: 14%;">SOURCE</th>
-                        <th style="width: 25%;">SUBJECT MATTER</th>
-                        <th style="width: 10%;">ACTION UNIT</th>
-                        <th style="width: 7%;">RECEIVED BY/DATE</th>
-                        <th style="width: 11%;">ACTION TAKEN</th>
-                        <th style="width: 8%;">DATE RELEASED</th>
-                        <th style="width: 7%;">REMARKS</th>
-                        <th style="width: 5%;">STORAGE</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($chunk as $log)
+@foreach ($chunks as $pageIndex => $chunk)
+    <div class="content">
+        <table class="dashboardTable">
+            <thead>
+                <tr>
+                    <th style="width: 2%;">CTRL #</th>
+                    <th style="width: 6%;">DATE RECEIVED</th>
+                    <th style="width: 14%;">SOURCE</th>
+                    <th style="width: 25%;">SUBJECT MATTER</th>
+                    <th style="width: 10%;">ACTION UNIT</th>
+                    <th style="width: 7%;">RECEIVED BY/DATE</th>
+                    <th style="width: 11%;">ACTION TAKEN</th>
+                    <th style="width: 8%;">DATE RELEASED</th>
+                    <th style="width: 7%;">REMARKS</th>
+                    <th style="width: 5%;">STORAGE</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($chunk as $item)
+                    @php $log = $item['log']; @endphp
+                    
+                    @if ($log)
                         @php
                             $exactSlip = $log->exactSlip ?? $log->routingSlip;
                             $exactDoc = $log->exactDoc ?? $log->document;
                         @endphp
                         <tr>
-                            <td>{{ $log->route_id == 0 ? 'N/A' : $log->route_id }}</td>
+                            <td>{{ $log->route_id }}</td>
                             <td>
                                 {{ $exactSlip && $exactSlip->date_received
                                     ? \Carbon\Carbon::parse($exactSlip->date_received)->format('M d, Y')
@@ -140,25 +145,20 @@
                                     $subject = implode(' ', array_slice($words, 0, 80)) . '...';
                                 }
                             @endphp
-                            <td style="max-width: 300px;font-size:6; overflow: hidden; text-overflow: ellipsis;"
-                                title="{{ $subject }}">
+                            <td style="max-width: 300px;font-size:6; overflow: hidden; text-overflow: ellipsis;" title="{{ $subject }}">
                                 {{ $subject }}
                             </td>
                             <td>{{ $exactSlip->pres_dept ?? 'N/A' }}</td>
-                            <td>{{ $exactSlip && $exactSlip->updated_at ? $exactSlip->updated_at->format('M d, Y') : 'N/A' }}
-                            </td>
+                            <td>{{ $exactSlip && $exactSlip->updated_at ? $exactSlip->updated_at->format('M d, Y') : 'N/A' }}</td>
                             <td>
                                 @if ($exactSlip && $exactSlip->r_destination)
-                                    <strong
-                                        class="text-danger">{{ ucwords(strtolower($exactSlip->r_destination)) }}</strong>
+                                    <strong class="text-danger">{{ ucwords(strtolower($exactSlip->r_destination)) }}</strong>
                                 @endif
                                 @if ($log->assigned_to)
-                                    , re-assigned to <strong
-                                        class="text-danger">{{ ucwords(strtolower($log->assigned_to)) }}</strong>
+                                    , re-assigned to <strong class="text-danger">{{ ucwords(strtolower($log->assigned_to)) }}</strong>
                                 @endif
                             </td>
-                            <td>{{ $exactDoc && $exactDoc->created_at ? $exactDoc->created_at->format('m-d-Y h:i:s A') : 'N/A' }}
-                            </td>
+                            <td>{{ $exactDoc && $exactDoc->created_at ? $exactDoc->created_at->format('m-d-Y h:i:s A') : 'N/A' }}</td>
                             <td style="font-size:10px;">
                                 @if ($exactSlip && !empty($exactSlip->trans_remarks))
                                     <span>{{ $exactSlip->trans_remarks }}</span>
@@ -166,25 +166,16 @@
                                 @if ($exactSlip && !empty($exactSlip->other_remarks))
                                     <span>{{ $exactSlip->other_remarks }}</span>
                                 @endif
-                                @php
-                                    $comment = $log->comments ?? '';
-                                    $wrappedComment = preg_replace('/((?:\S+\s+){4})/', '$1<br>', $comment);
-                                @endphp
+                                @php $comment = $log->comments ?? ''; $wrappedComment = preg_replace('/((?:\S+\s+){4})/', '$1<br>', $comment); @endphp
                                 @if ($comment)
-                                    <span
-                                        style="margin-top:2px; max-width:150px; word-wrap:break-word; white-space:normal;">
-                                        {!! $wrappedComment !!}
-                                    </span>
+                                    <span style="margin-top:2px; max-width:150px; word-wrap:break-word; white-space:normal;">{!! $wrappedComment !!}</span>
                                 @endif
                             </td>
                             <td></td>
                         </tr>
-                    @endforeach
-
-                    {{-- Fill remaining rows to keep 15 rows per page --}}
-                    @for ($i = count($chunk); $i < $logsPerPage; $i++)
+                    @else
                         <tr>
-                            <td>&nbsp;</td>
+                            <td>{{ $item['route_id'] }}</td>
                             <td>&nbsp;</td>
                             <td>&nbsp;</td>
                             <td>&nbsp;</td>
@@ -195,14 +186,29 @@
                             <td>&nbsp;</td>
                             <td>&nbsp;</td>
                         </tr>
-                    @endfor
-                </tbody>
-            </table>
-        </div>
+                    @endif
+                @endforeach
 
-        {{-- Page break except for last page --}}
-        @if ($pageIndex + 1 < $totalPages)
-            <div class="page-break"></div>
-        @endif
-    @endforeach
+                @for ($i = count($chunk); $i < $logsPerPage; $i++)
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
+
+    @if ($pageIndex + 1 < $totalPages)
+        <div class="page-break"></div>
+    @endif
+@endforeach
 </body>
