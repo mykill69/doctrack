153906475156d80aacd0cf29c8379f1836d3f249
mykill69 <baliviamichael@gmail.com>
2026-08-10 10:20:35 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ use App\Http\Controllers\DoctrackController;
 use App\Http\Controllers\PrintController;
 
 Route::group(['middleware'=>['guest']],function(){
-    // Route::get('/', function () {
-    //     return view('.login.login');
-    // });
+    Route::get('/', function () {
+        return view('.login.login');
+    });
 
     Route::get('/login', [LoginAuthController::class, 'getLogin'])->name('getLogin');
     Route::post('/login', [LoginAuthController::class, 'postLogin'])->name('postLogin');
